a778ef578a5a40bc74aa1bfafd67d62c0494b65a
Vitaly Engel <vitaliy.engel@gmail.com>
2022-05-30 22:05:25 +0300

    sass less, auto reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,79 @@
-// Определяем константы Gulp
-const { src, dest, parallel, series, watch } = require('gulp');
+	// Определяем переменную "preprocessor"
+	let preprocessor = 'sass'; // Выбор препроцессора в проекте - sass или less
 
-// Подключаем Browsersync
-const browserSync = require('browser-sync').create();
+	// Определяем константы Gulp
+	const { src, dest, parallel, series, watch } = require('gulp');
 
-// Подключаем gulp-concat
-const concat = require('gulp-concat');
+	// Подключаем Browsersync
+	const browserSync = require('browser-sync').create();
 
-// Подключаем gulp-uglify-es
-const uglify = require('gulp-uglify-es').default;
+	// Подключаем gulp-concat
+	const concat = require('gulp-concat');
 
-// Определяем логику работы Browsersync
-function browsersync() {
-    browserSync.init({ // Инициализация Browsersync
-        server: { baseDir: 'app/' }, // Указываем папку сервера
-        notify: false, // Отключаем уведомления
-        online: true // Режим работы: true или false
-    })
-}
+	// Подключаем gulp-uglify-es
+	const uglify = require('gulp-uglify-es').default;
 
-function scripts() {
-    return src([ // Берем файлы из источников
-            'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
-            'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
-        ])
-        .pipe(concat('app.min.js')) // Конкатенируем в один файл
-        .pipe(uglify()) // Сжимаем JavaScript
-        .pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
-        .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
-}
+	// Подключаем модули gulp-sass и gulp-less
+	const sass = require('gulp-sass')(require('sass'));
+	const less = require('gulp-less');
 
-function startwatch() {
+	// Подключаем Autoprefixer
+	const autoprefixer = require('gulp-autoprefixer');
 
-    // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
-    watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+	// Подключаем модуль gulp-clean-css
+	const cleancss = require('gulp-clean-css');
 
-}
+	// Определяем логику работы Browsersync
+	function browsersync() {
+	    browserSync.init({ // Инициализация Browsersync
+	        server: { baseDir: 'app/' }, // Указываем папку сервера
+	        notify: false, // Отключаем уведомления
+	        online: true // Режим работы: true или false
+	    })
+	}
 
-// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
-exports.browsersync = browsersync;
+	function scripts() {
+	    return src([ // Берем файлы из источников
+	            'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+	            'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+	        ])
+	        .pipe(concat('app.min.js')) // Конкатенируем в один файл
+	        .pipe(uglify()) // Сжимаем JavaScript
+	        .pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
+	        .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+	}
 
-// Экспортируем функцию scripts() в таск scripts
-exports.scripts = scripts;
+	function startwatch() {
 
-// Экспортируем дефолтный таск с нужным набором функций
-exports.default = parallel(scripts, browsersync, startwatch);
+	    // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
+	    watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+
+	    // Мониторим файлы препроцессора на изменения
+	    watch('app/**/' + preprocessor + '/**/*', styles);
+
+	    // Мониторим файлы HTML на изменения
+	    watch('app/**/*.html').on('change', browserSync.reload);
+
+	}
+
+	function styles() {
+	    return src('app/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+	        .pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
+	        .pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+	        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+	        .pipe(cleancss({ level: { 1: { specialComments: 0 } } /* , format: 'beautify' */ })) // Минифицируем стили
+	        .pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
+	        .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+	}
+
+	// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
+	exports.browsersync = browsersync;
+
+	// Экспортируем функцию scripts() в таск scripts
+	exports.scripts = scripts;
+
+	// Экспортируем функцию styles() в таск styles
+	exports.styles = styles;
+
+	// Экспортируем дефолтный таск с нужным набором функций
+	exports.default = parallel(styles, scripts, browsersync, startwatch);
