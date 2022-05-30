cf53abf99b32ed33f05c77b081a08db6496da595
Vitaly Engel <vitaliy.engel@gmail.com>
2022-05-30 14:31:16 +0300

    browsersync installed, index.html added

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// Определяем константы Gulp
+const { src, dest, parallel, series, watch } = require('gulp');
+
+// Подключаем Browsersync
+const browserSync = require('browser-sync').create();
+
+// Определяем логику работы Browsersync
+function browsersync() {
+    browserSync.init({ // Инициализация Browsersync
+        server: { baseDir: 'app/' }, // Указываем папку сервера
+        notify: false, // Отключаем уведомления
+        online: true // Режим работы: true или false
+    })
+}
+
+// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
+exports.browsersync = browsersync;
