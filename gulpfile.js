3de7bb25d5616f9d8677ff16c78722521e01ffa5
Vitaly Engel <vitaliy.engel@gmail.com>
2022-05-30 17:32:11 +0300

    gulp - js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ const { src, dest, parallel, series, watch } = require('gulp');
 // Подключаем Browsersync
 const browserSync = require('browser-sync').create();
 
+// Подключаем gulp-concat
+const concat = require('gulp-concat');
+
+// Подключаем gulp-uglify-es
+const uglify = require('gulp-uglify-es').default;
+
 // Определяем логику работы Browsersync
 function browsersync() {
     browserSync.init({ // Инициализация Browsersync
@@ -13,5 +19,29 @@ function browsersync() {
     })
 }
 
+function scripts() {
+    return src([ // Берем файлы из источников
+            'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+            'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+        ])
+        .pipe(concat('app.min.js')) // Конкатенируем в один файл
+        .pipe(uglify()) // Сжимаем JavaScript
+        .pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
+        .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+}
+
+function startwatch() {
+
+    // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
+    watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+
+}
+
 // Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
 exports.browsersync = browsersync;
+
+// Экспортируем функцию scripts() в таск scripts
+exports.scripts = scripts;
+
+// Экспортируем дефолтный таск с нужным набором функций
+exports.default = parallel(scripts, browsersync, startwatch);
